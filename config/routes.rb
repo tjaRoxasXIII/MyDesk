8426e6342c0db149c067979e912d4f98da97cb04
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-10-31 10:47:25 -0400

    Adding Admin from Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :tickets
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root to: "tickets#index"
 end
