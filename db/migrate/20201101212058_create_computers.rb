6e41f28cbb555b10ffe4578b594fa647cc4d163d
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-11-02 10:29:24 -0500

    Re-thought the User/Admin model to possibly join into single object.  Started building Seed data for computers and tickets

--- db/migrate/20201101212058_create_computers.rb
+++ db/migrate/20201101212058_create_computers.rb
@@ -1,9 +1,10 @@
 class CreateComputers < ActiveRecord::Migration[6.0]
   def change
     create_table :computers do |t|
-      t.string :type
+      t.string :device_type
       t.string :service_tag
       t.string :os
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_183445) do
+ActiveRecord::Schema.define(version: 2020_11_01_212058) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_10_31_183445) do
     t.string "last_name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "computers", force: :cascade do |t|
+    t.string "device_type"
+    t.string "service_tag"
+    t.string "os"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -49,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_31_183445) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
