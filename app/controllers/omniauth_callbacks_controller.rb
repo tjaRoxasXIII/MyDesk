15744f4ebf4d74ed9895af42d69a97f57d274dcb
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-11-06 16:15:14 -0500

    Adding line 'skip_jwt: true' to fix Google OAuth timeserver errors

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    before_action :authenticate_user!, except: :google
+    before_action :authenticate_user!, except: :google 
 
     def google_oauth2
         @user = User.google_omniauth(request.env['omniauth.auth'])
